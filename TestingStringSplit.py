821ec1cc94267b78d903d640f1df8b2cf659abb7
KristieAW <119287153+KristieAW@users.noreply.github.com>
2023-01-23 20:36:56 +1300

    Further research
    Practicing splitting string

---
+++ TestingStringSplit.py
@@ -0,0 +1,22 @@
+# TestingStringSplit.py
+#
+# Author Kristie
+# date 11.01.2023
+
+class Person:
+    #class code containing data and behaviour will come here
+    def __init__(self,age,gender):
+        self.age = age
+        self.gender = gender
+
+#create a new object of person class
+harry=Person(36,"male")
+sarrah=Person(34,"female")
+
+#output: 10
+
+print(harry.age)
+print(sarrah.age)
+
+
+            
